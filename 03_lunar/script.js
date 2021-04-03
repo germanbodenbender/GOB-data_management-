cce7b0ce77afe854a0f0037fd795ac1a4c4236cf
German Otto Bodenbender <german.bodenbender@gmail.com>
2021-04-03 21:10:09 +0200

    Update script.js

--- 03_lunar/script.js
+++ 03_lunar/script.js
@@ -6,7 +6,7 @@ import { color, GUI } from './dat.gui.module.js'
 
 let camera, scene, raycaster, renderer, gui
 const mouse = new THREE.Vector2()
-
+window.addEventListener( 'click', onClick, false);
 
 init()
 animate()
@@ -56,8 +56,71 @@ function init() {
 
 }
 
+function onClick( event ) {
 
+    console.log( `click! (${event.clientX}, ${event.clientY})`)
 
+	// calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    raycaster.setFromCamera( mouse, camera )
+
+	// calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObjects( scene.children, true )
+
+    let container = document.getElementById( 'container' )
+    if (container) container.remove()
+
+    // reset object colours
+    //scene.traverse((child, i) => {
+    //   if (child.isMesh) {
+    //        child.material.color.set( 'white' )
+    //    }
+    //});
+
+    if (intersects.length > 0) {
+
+        // get closest object
+        const object = intersects[0].object
+        console.log(object) // debug
+
+        //object.material.color.set( 'yellow' )
+
+        // get user strings
+        let data, count
+        if (object.userData.attributes !== undefined) {
+            data = object.userData.attributes.userStrings
+        } else {
+            // breps store user strings differently...
+            data = object.parent.userData.attributes.userStrings
+        }
+
+        // do nothing if no user strings
+        if ( data === undefined ) return
+
+        console.log( data )
+        
+        // create container div with table inside
+        container = document.createElement( 'div' )
+        container.id = 'container'
+        
+        const table = document.createElement( 'table' )
+        container.appendChild( table )
+
+        for ( let i = 0; i < data.length; i ++ ) {
+
+            const row = document.createElement( 'tr' )
+            row.innerHTML = `<td>${data[ i ][ 0 ]}</td><td>${data[ i ][ 1 ]}</td>`
+            table.appendChild( row )
+        }
+
+        document.body.appendChild( container )
+    }
+
+}
 
 function animate() {
 
